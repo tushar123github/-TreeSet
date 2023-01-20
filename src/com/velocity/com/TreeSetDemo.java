68e5124f42af735700aa2fe85c1a9cd8e170a683
tushar <tusharjadhav9997@gmail.com>
2023-01-20 14:27:58 +0530

    add file

---
+++ src/com/velocity/com/TreeSetDemo.java
@@ -0,0 +1,17 @@
+package com.velocity.com;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		
+		TreeSet hs = new TreeSet();
+		hs.add(120);
+		hs.add(453);
+		hs.add(789);
+		System.out.println(hs);
+
+	}
+
+}
